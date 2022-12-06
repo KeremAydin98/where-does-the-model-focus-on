611fce68525652d15ab411ee179cfed68404257c
KeremAydin98 <aydinke16@itu.edu.tr>
2022-12-06 10:00:03 +0300

    train batch

--- main.py
+++ main.py
@@ -16,4 +16,30 @@ train_dl = DataLoader(train_dataset, batch_size=32, collate_fn=train_dataset.col
 val_dl = DataLoader(val_dataset, batch_size=32, collate_fn=val_dataset.collate_fn)
 
 
+def train_batch(model, data, optimizer, criterion):
+
+    # Prepare the model for training
+    model.train()
+
+    # Extract images and labels from data separately
+    imgs, labels, _ = data
+
+    # Forward propagation
+    preds = model(imgs)
+
+    optimizer.zero_grad()
+
+    # Loss and accuracy
+    loss, acc = criterion(preds, labels)
+
+    # Gradient computation
+    loss.backward()
+
+    # Backward propagation
+    optimizer.step()
+
+    return loss.item(), acc.item()
+
+
+
 
