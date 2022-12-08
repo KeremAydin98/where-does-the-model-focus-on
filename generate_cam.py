df22b4aea93cf4dce570921c0031ad2077f9b779
KeremAydin98 <aydinke16@itu.edu.tr>
2022-12-08 12:19:40 +0300

    first images shown

--- generate_cam.py
+++ generate_cam.py
@@ -2,7 +2,9 @@ import config
 from models import *
 from preprocessing import *
 from glob import glob
-import torchvision
+import cv2
+import matplotlib.pyplot as plt
+from torchvision import transforms as T
 
 device = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
 
@@ -22,7 +24,8 @@ img2fmap = nn.Sequential(*(list(model.model[:4]) + list(model.model[4][:2])))
 # Test path
 test_files = glob(config.test_path)
 # Load test dataset
-test_ds = FruitImages(test_files, device=device)
+test_ds = FruitImages(test_files, transform=val_tf, device=device)
+test_dl = DataLoader(test_ds, batch_size=20, shuffle=True, collate_fn=test_ds.collate_fn)
 
 
 def img2cam(x):
@@ -67,3 +70,44 @@ def preprocess_for_cam(img):
 
 
 heatmap = img2cam(preprocess_for_cam(test_ds[0][0]))
+
+def upsampleHeatMap(map, img):
+
+    image_size = 128
+
+    min, max = map.min(), map.max()
+
+    #map = 255 * ((map - min) / (max-min))
+
+    #map = torch.tensor(map)
+
+    map = np.uint8(map)
+
+    map = cv2.resize(map, (image_size, image_size))
+
+
+    map = cv2.applyColorMap(255-map, cv2.COLORMAP_JET)
+    map = np.uint8(map)
+
+    return np.uint8(map*0.7 + img*0.3)
+
+transform = T.Resize((128, 128))
+
+x, y, z = next(iter(test_dl))
+
+f, axs = plt.subplots(2,4)
+
+for i in range(4):
+
+    image = np.array(transform(z[i]).permute(1,2,0))
+
+    heatmap = img2cam(torch.tensor(x[i]).float().expand(1,3,128,128))
+    heatmap = upsampleHeatMap(heatmap, image)
+
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    heatmap = cv2.cvtColor(heatmap, cv2.COLOR_BGR2RGB)
+
+    axs[0,i].imshow(heatmap)
+    axs[1,i].imshow(image)
+
+plt.show()
